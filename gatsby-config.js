69f652f84829efd5b4f0dca5ff65cad8d65370ef
Fredy Whatley (fwhatley) <fredywhatley@gmail.com>
2021-07-07 15:42:44 -0400

    base code

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,15 +3,6 @@ module.exports = {
         siteUrl: `https://www.yourdomain.tld`,
     },
     plugins: [
-        {
-            resolve: 'gatsby-plugin-launchdarkly',
-            options: {
-              clientSideID: '<your-launchdarkly-project-client-side-id>',
-              options: {
-                // any LaunchDarkly options you may want to implement
-                bootstrap: 'localstorage', // caches flag values in localstorage
-              },
-            },
-          },
+
     ]
 }
