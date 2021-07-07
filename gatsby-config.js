cef624eebdb3d71bf8aa8b180672bb2181f2bd1c
Fredy Whatley (fwhatley) <fredywhatley@gmail.com>
2021-07-07 17:05:21 -0400

    add launch darkly

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,15 @@ module.exports = {
         siteUrl: `https://www.yourdomain.tld`,
     },
     plugins: [
-
+        {
+            resolve: 'gatsby-plugin-launchdarkly',
+            options: {
+                clientSideID: '',
+                options: {
+                    // any LaunchDarkly options you may want to implement
+                    bootstrap: 'localstorage', // caches flag values in localstorage
+                },
+            },
+        },
     ]
 }
